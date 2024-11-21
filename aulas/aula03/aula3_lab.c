66a003d25bc134592d1efc025c1c2298f451ce45
mfs6 <mfs6@cin.ufpe.br>
2024-11-21 00:41:25 -0300

    adicionado mais comentários

--- aulas/aula03/aula3_lab.c
+++ aulas/aula03/aula3_lab.c
@@ -30,8 +30,7 @@ void main()
     // O maior inteiro positivo escrito em binário equivale a 9223372036854775807 = (2^63)-1
     // Para escrever um long em binário usa o 0b como em verilog
 
-    printf("\nl = %d\n", l); /* Dá um warning que avisa que o número l pode não caber num int que tem a marcação %d,
-    aparentemente resulta em -1 sempre que não couber*/
+    printf("\nl = %d\n", l); /* Dá um warning que avisa que o número l pode não caber num int que tem a marcação %d, aparentemente resulta em -1 sempre que não couber*/
     // Claro que nesse exemplo não cabe porque l é o maior long possível
     printf("l = %ld\n", l); // O ideal é usar a marcação %ld
 
@@ -40,24 +39,27 @@ void main()
 
     int i = 1024;
     printf("\nValor de i = %d\n", i);
-    printf("Tamanho de i = %ld\n", sizeof(i)); // Exemplo do sizeof() que retorna o máximo de bytes que o tipo da variavel do parametro pode conter
-    printf("Endereço de i = %ld\n", &i);       // Exemplo de como encontrar o endereço da memória que contem o valor da variavel (usar &)
+    printf("Tamanho de i = %ld\n", sizeof(i)); // Exemplo do sizeof() que retorna o máximo de bytes que o tipo da variável do parametro pode conter
+    printf("Endereço de i = %ld\n", &i);       // Exemplo de como encontrar o endereço da memória que contem o valor da variável (usar a marcação &)
 
     /*-------------------------PONTEIROS-------------------------*/
-    int *p = &i; // Declaração de um ponteiro p para o int i porque usa o &
-    // O ponteiro armazena o endereço da memória onde está o valor da variavel, não o próprio valor dela, porque usa o &
+    int *p = &i; // Declaração de um ponteiro p para o int i porque usa o "tipo" int*
+    // O ponteiro (que é p) armazena o endereço da memória onde está o valor da variável, não o próprio valor dela, porque usa o &
+    // Porém o ponteiro p armazena também um *p que é o valor da variável ao qual p aponta
 
-    printf("\nValor de p = %ld\n", p);
+    printf("\nValor de p = %ld\n", p);        // Printa o endereço de memória da variável que p aponta (no debugger apareçe em hexadecimal)
     printf("Tamanho de p = %ld\n", sizeof(p)); // sizeof = 8 bytes = 64 bits -> tamanho da memoria para cada "bloco de dados"
-    printf("Endereço de p = %ld\n", &p);       // O ponteiro também tem o seu endereço na memória
+    printf("Endereço de p = %ld\n", &p);      // O ponteiro também tem o seu endereço na memória
 
     int k = 1953;
-    *p = k; // Pega o valor da variavel que o ponteiro aponta porque não tem o &
+    *p = k; // Coloca o valor de k na variável que o ponteiro p aponta (no caso o i) e não no ponteiro p em si
+
+    printf("\nValor de i = %d\n", i); // Print do i modificado pelo *p = k
 
     printf("\nValor de k = %d\n", k);
     printf("Tamanho de k = %ld\n", sizeof(k));
     printf("Endereço de k = %ld\n", &k); // Endereço na memória de k, porque usa o &
 
-    printf("\nValor de *p = %ld\n", *p);  // O valor de p mudou para o valor de k -> *p = k, antes ele tinha o endereço de memória de i
+    printf("\nValor de *p = %ld\n", *p); // O valor de *p mudou para o valor de k, antes *p tinha o valor de i
     printf("Endereço de p = %ld\n", &p); // O ponteiro também tem o seu endereço na memória e ele não mudou
 }
