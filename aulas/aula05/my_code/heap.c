c9e1e8d1621a26bcea383fb128908cdc959a4d3b
mfs6 <mfs6@cin.ufpe.br>
2024-11-27 01:33:39 -0300

    polida da aula05

--- aulas/aula05/my_code/heap.c
+++ aulas/aula05/my_code/heap.c
@@ -1,43 +1,57 @@
+#include <stdio.h>
 #include <stdlib.h>
-
-// void a(int x)
-// {
-//     int *p = (int *)malloc(sizeof(int)); /* coloca em *p o endereço retornado
-//      por malloc que tem o tamanho de um int (4 bytes)*/
-//     /* faz o cast pra ponteiro do tipo int pq malloc retorna um ponteiro tipo void,
-//     e p é um ponteiro do tipo int*/
-//     *p = x;
-//     printf("p = %p\n", p);
-//     printf("*p = %d\n", *p);
-// }
 
 int *a(int x)
 {
-    int *p = (int *)malloc(sizeof(int)); /* coloca em *p o endereço retornado
-     por malloc que tem o tamanho de um int (4 bytes)*/
-    /* faz o cast pra ponteiro do tipo int pq malloc retorna um ponteiro tipo void,
-    e p é um ponteiro do tipo int*/
-    *p = x;
-    printf("p = %p\n", p);
+    int *p = (int *)malloc(sizeof(int)); /* Declara um ponteiro p do tipo int e coloca em p
+    o endereço retornado por malloc que tem o tamanho de um int (4 bytes) */
+    /* Faz o cast pra ponteiro do tipo int pq malloc retorna um ponteiro tipo void,
+    e p é um ponteiro do tipo int */
+
+    *p = x; // Atribui ao valor do ponteiro p (*p) o valor x recebido como parâmetro
+    
+    printf("\np = %p\n", p);
     printf("*p = %d\n", *p);
 
     return p;
 }
 
+void b(int x)
+{
+    int *p = (int *)malloc(sizeof(int)); /* Declara um ponteiro p do tipo int e coloca em p
+    o endereço retornado por malloc que tem o tamanho de um int (4 bytes) */
+    /* Faz o cast pra ponteiro do tipo int pq malloc retorna um ponteiro tipo void,
+    e p é um ponteiro do tipo int */
+
+    *p = x; // Atribui ao valor do ponteiro p (*p) o valor x recebido como parâmetro
+
+    printf("\np = %p\n", p);
+    printf("*p = %d\n", *p);
+}
+
 void main()
 {
-    //a(12345);
-
-    //int *q;
     int *q = a(12345);
 
-    *q = 54321;
+    printf("\nDepois de chamar a:\n");
+    printf("\nq = %p\n", q);
+    printf("*q = %d\n", *q);
 
-    printf("antes de sair (*q) %d\n", *q);
-    printf("antes de sair (q) %p\n", q);
+    printf("\nDepois de chamar b:\n"); // Printa antes pq b é void e os prints estão dentro dele
+    b(12345);
 
-    free(q); // Limpa o valor (*q) do endereço de memória que q aponta
+    *q = 54321; // muda apenas o valor de q (*q)
 
-    printf("antes de sair (q) %p\n", q);
-    printf("antes de sair (*q) %d\n", *q); // por isso printa um valor aleatório (algo como um valor null)
+    printf("\nApós mudar o valor de q, ou seja, *q");
+    printf("\nq = %p\n", q);
+    printf("*q = %d\n", *q);
+
+    free(q); // Limpa o endereço de memória que q aponta, ou seja, limpa o valor (*q)
+
+    printf("\nDepois de liberar q:\n");
+    printf("q = %p\n", q);   /* O endereço continua o mesmo (endereço de p)
+    que foi retornado pelo malloc da função a mas o valor de q (*q) muda */
+    printf("*q = %d\n", *q); /* Por isso printa um valor aleatório (definido pelo SO)
+    igual quando inicializa uma váriavel implicitamente, isto é, sem declarar valor */
+    /*Em Java cada tipo tem um valor padrão especifico como o int que é 0 por padrão */
 }
--- aulas/aula05/my_code/memo.c
+++ aulas/aula05/my_code/memo.c
@@ -3,43 +3,37 @@
 
 double pi = 3.1415;
 
-double area(double r)
+double area(double r) // Área do círculo
 {
-
-    return pi * r * r;
+    return pi * r * r; // π*r²
 }
 
 void main()
 {
-
     double x = 10;
-    printf("&x = %p\n", &x); // printa o endereço de x
+    printf("&x = %p\n", &x); // Printa o endereço de x
 
-    double *p = &pi;
-    printf("p = %p\n", p);   // printa o endereço de pi, não o valor que é *p = 3,1415
-    printf("*p = %lf\n", *p); // printa o valor que *p aponta, o valor de pi = 3,1415
+    double *p = &pi;          // Ponteiro p do tipo double
+    printf("\np = %p\n", p);  // Printa o endereço de pi, não o valor que é *p = 3,1415
+    printf("*p = %lf\n", *p); // Printa o valor que p aponta, *p = valor de pi = 3,1415
 
-    void *v = malloc(sizeof(double)); /* executa malloc, reserva memoria
-    no tamanho do double (8 bytes se não me engano) e armazena no ponteiro v, malloc retorna um ponteiro*/
+    void *v = malloc(sizeof(double)); /* Executa malloc, reserva 8 bytes de memoria (tamanho do double)
+    e armazena no ponteiro v, malloc retorna um ponteiro do "tipo" void*/
     double *d = v;
-    // double *d = (double *)v --> é redundante
+    // double *d = (double *)v --> É redundante fazer o cast explicito do ponteiro do "tipo" void pro ponteiro do tipo double
 
-    //printf("d = %lf\n", d);
+    printf("\nv = %p\n", v); // Mostra o endereço de memória reservada pelo malloc
 
-    printf("v = %p\n", v); // mostra o endereço de memória reservado pelo malloc
+    printf("*v = %lf\n", *(double *)(v)); // Faz o cast de v para um ponteiro do tipo double e mostra o valor de v (*v)
 
-    printf("*v = %lf\n", *(double *)(v)); // faz o cast de v para um ponteiro de um double e mostra o valor v (*v)
-
-    printf("*d = %lf\n", *d); /* igual a printf("v = %lf\n", *(double *)(v)); // faz o
-    cast de v para um ponteiro de um double*/
-
-    // v é um ponteiro pra um void
-    // interprete v como um ponteiro pra em double
-    //**(double * )(v) valor que está armazenado no endereço void *v
+    printf("*d = %lf\n", *d); // Igual a printf("v = %lf\n", *(double *)(v));(L27) pois: double *d = v;(22)
 
     *d = pi;
-    printf("\n*d = %lf\n", *d); //  agora printa o valor de pi
+    printf("\n*d = %lf\n", *d); // Agora printa o valor de pi
 
-    free(v); // liberar a memória do ponteiro v
-    free(v);
+    free(v); // Libera a memória do ponteiro v
+
+    printf("*v = %ld\n", &v); /* Por isso printa um valor aleatório (definido pelo SO)
+    igual quando inicializa uma váriavel implicitamente, isto é, sem declarar valor */
+    /*Em Java cada tipo tem um valor padrão especifico como o int que é 0 por padrão */
 }
