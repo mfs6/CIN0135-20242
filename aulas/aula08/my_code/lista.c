b8ef61bd7e7e8f648ee950a681480a5a8b339061
mfs6 <mfs6@cin.ufpe.br>
2024-12-06 03:33:05 -0300

    comentários adicionados em casa

--- aulas/aula08/my_code/lista.c
+++ aulas/aula08/my_code/lista.c
@@ -1,68 +1,144 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node /* Lista */
+//// Define o nó (a caixinha) da lista encadeada (semelhante a Java)
+struct Node
 {
-    int val;
+    //// Atributos de cada nó (de cada caixinha)
+    int index; //* Adicionado por mim
+    int valor;
+
+    //// Ponteiro para o próximo nó da lista encadeada
     struct Node *next;
+    //! Lembrar que é um ponteiro do tipo Node (que é o próprio nó) --- (parece uma função recursiva)
 };
 
-typedef struct Node Node; /* Apelido da lista */
+//// Atribuindo o apelido "No" para a "struct Node", que é o nó da lista encadeada
+typedef struct Node No;
+//! Daqui em diante, sempre que for me referir a "struct Node", será por seu apelido "No"
 
-Node *create_list() /* Cria a lista definida acima */
+//// Finalmente cria a lista que é composta pelos nós (por cada caixinha) da struct "Node"
+No *create_list()
 {
-    Node *head = malloc(sizeof(Node)); /* Aloca a memória para o ponteiro head */
-    (*head).val = 165;                 /* Define o valor guardado no endereço de memória em head aponta */
-    (*head).next = NULL;               /* Define o próximo com NULL, já que não tem próximos elementos */
+    //// Cria o ponteiro head (que representa o início da lista) e aloca a memória para ele
+    No *head = malloc(sizeof(No));
+
+    //// Define o valor guardado no endereço de memória em que head aponta (na prática cria a primeira caixinha da lista)
+    (*head).index = 0;    //* Adicionado por mim
+    (*head).valor = 1652; //? Por que está (*head) como se fosse um cast?
+
+    //// Define a próxima caixinha como NULL, já que na criação (inicialização) da lista não tem próximos elementos
+    (*head).next = NULL;
+
+    //// Retorna o início da lista (a primeira caixinha). ou seja, na prática cria de fato a lista
     return head;
 }
 
-// Define a função que coloca no final da lista um dado valor
-void append(Node *head, int valor) /* Recebe a "cabeça" da lista */
-{
-    // Define onde começa a procura, inicializado como a "cabeça" da lista
-    Node *cursor = head; // Dessa forma o head não muda durante a função, quem muda é o cursor para iterar na lista
+//! Lembrar que a lista encadeada é basicamente ponteiros para os nós
 
-    while (cursor->next != NULL) /* Até achar o valor que é NULL e então adicionar o valor no lugar do NULL */
+//// Define a função que insere elementos no "tail" (final) da lista
+void append(No *lista, int valor)
+{
+    //// Define um cursor que vai começar a procurar o "tail" (final) da "lista", para inserir o "valor" nele.
+    No *cursor = lista;
+
+    int index = 1; // * Variável pra contar o index
+
+    //// O cursor itera até achar o ponteiro (*next) que é NULL e então adiciona o "valor" no lugar do NULL
+    while (cursor->next != NULL)
     {
+        cursor = cursor->next; /* O normal seria cursor.next, mas o vscode adota essa notação */
+        index++;               //* Incrementa index pra ir contando a posição
+    }
+
+    //// Depois de achar onde é NULL, sai do while e começa a inserção do parâmetro "valor"
+
+    //// Cria o ponteiro tail (que representa o final da lista) e aloca a memória para ele
+    No *tail = malloc(sizeof(No));
+
+    //// Altera os valores
+    tail->index = index;
+    tail->valor = valor;
+    tail->next = NULL; //? DEBUG: Essa linha é redundante sempre?
+
+    /* Posiciona o cursor já no "tail" que foi definido com NULL logo acima, pra não precisar iterar numa nova chamada da função append */
+    cursor->next = tail;
+    //? Pra que serve essa linha acima? DEBUG: após a execução dela que a lista de fato incorpora a valor da cauda (talvez por causa de No *cursor = lista;)?
+}
+
+void print_list(No *lista) //* Adicionado por mim
+{
+    No *cursor = lista;
+
+    while (cursor != NULL)
+    {
+        printf("Posição: %d, ", cursor->index);
+        printf("Valor: %d\n", cursor->valor);
         cursor = cursor->next;
     }
-    Node *tail = malloc(sizeof(int));
-    tail->val = valor;
-    tail->next = NULL;
-    cursor->next = tail;
 }
 
 void main()
 {
-    // Exemplo 1 (mais direto ao ponto)
-    // Node first;
-    // Node second;
-    // Node third;
-    // Node four;
+    //// Começa criando a lista, chamando a função apropriada
+    No *lista = create_list(); //// Nesse momento a lista só tem a primeira caixinha, o head
 
-    // first.val = 2;
-    // first.next = &second; /* & que faz referencia ao endereço de memoria ao qual Node *next aponta */
+    print_list(lista); //* Adicionado por mim
 
-    // second.val = 3;
-    // second.next = &third;
+    printf("\n");
 
-    // third.val = 5;
-    // third.next = &four;
+    //// Insere valores na lista, através da função append, que insere os valores no final da lista
+    append(lista, 2);
+    append(lista, 3);
+    append(lista, 5);
+    append(lista, 7);
 
-    // four.val = 7;
-    // four.next = NULL;
-
-    // Exemplo 2 (via create_list)
-    Node *head = create_list();
-
-    printf("%d\n", (*head)); /* Printa o valor apontado pelo ponteiro head definido em create_list */
-
-    /* Coloca no final da lista um dado valor */
-    append(head, 2);
-    append(head, 3);
-    append(head, 5);
-    append(head, 7);
-
-    // Debugar melhor em casa
+    print_list(lista); //* Adicionado por mim
 }
+
+//// Ilustração das caixinhas de uma lista encadeada
+
+// Elemento 0 (Head) -> Primeiro da lista
+// ------------------------------------------
+// |             |            |             |
+// |             |            |  Ponteiro   |
+// |   index=0   |  Valor=0   |   para o    |
+// |             |            |  próximo = 1|
+// |             |            |             |
+// ------------------------------------------
+
+// Elemento 1
+// ------------------------------------------
+// |             |            |             |
+// |             |            |  Ponteiro   |
+// |   index=1   |  valor=2   |   para o    |
+// |             |            |  próximo=2  |
+// |             |            |             |
+// ------------------------------------------
+
+// Elemento 2
+// ------------------------------------------
+// |             |            |             |
+// |             |            |  Ponteiro   |
+// |   index=2   |  valor=3   |   para o    |
+// |             |            |  próximo=3  |
+// |             |            |             |
+// ------------------------------------------
+
+// Elemento 3
+// ------------------------------------------
+// |             |            |             |
+// |             |            |  Ponteiro   |
+// |   index=3   |  valor=5   |   para o    |
+// |             |            |  próximo=4  |
+// |             |            |             |
+// ------------------------------------------
+
+// Elemento 4 (Tail) -> Último da lista, portanto aponta pra NULL
+// ------------------------------------------
+// |             |            |             |
+// |             |            |  Ponteiro   |
+// |   index=4   |  valor=7   |   para o    |
+// |             |            |  próximo =  |
+// |             |            |    NULL     |
+// ------------------------------------------
